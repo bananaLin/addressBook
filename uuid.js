414062c6ba9032aabcb9dda24676f70b018f4cbc
bananaLin <527929821@qq.com>
2020-02-28 17:28:57 +0800

    delete some files

--- uuid.js
+++
@@ -1,6 +0,0 @@
-var uuid = require('node-uuid');
-console.log(uuid.v1())
-console.log(uuid.v4())
-console.log(uuid.v4().replace(/-/g, '').replace(/[A-Za-z]/g, ''))
-
-console.log(new Date().getTime())
